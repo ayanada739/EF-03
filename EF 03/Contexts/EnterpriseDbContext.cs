22660b94ad2f977718e69293c0cdc8653dccb6d0
ayanada739 <ayanada739@gmail.com>
2024-08-29 19:39:30 +0300

    Many To Many Relationship Mapping

---
+++ EF 03/Contexts/EnterpriseDbContext.cs
@@ -0,0 +1,51 @@
+﻿using EF_03.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_03.Contexts
+{
+    internal class EnterpriseDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+          =>   //optionsBuilder.UseSqlServer("Data source = . ; Initial Catalog = Enterprise; Integarted Security = true") //old
+            optionsBuilder.UseSqlServer("Server = .; Database = Enterprise; Trusted_Connection = true");
+
+
+
+        public DbSet<Student> Student { get; set; }
+
+        public DbSet<Course> Course { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            //modelBuilder.Entity<Student>()
+            // .HasMany(S => S.Course)
+            // .WithMany(C => C.Student);
+            //no Represenation For Many To Many Relation In Database
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(SC => new { SC.StudentId, SC.CourseId });
+            //Composite PK
+
+            modelBuilder.Entity<Student>()
+                .HasMany(S => S.StudentCourses)
+                .WithOne(SC => SC.Student)
+                .IsRequired(true)
+                .HasForeignKey(SC => SC.StudentId);
+
+            modelBuilder.Entity<Course>()
+                .HasMany(C => C.StudentCourses)
+                .WithOne(SC => SC.Course);
+                 
+        }
+    }
+}
---
+++ EF 03/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_03.Entities
+{
+    internal class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        //public ICollection<Student> Student { get; set; } = new HashSet<Student>();
+        //Navigtional Property (Many)
+        public ICollection<StudentCourse> StudentCourses { get; set; } = new HashSet<StudentCourse>();
+
+    }
+}
